3ab6725c833fe48383d898246f725faa1b2c17da
Yuto Kawamura <kawamuray.dadada@gmail.com>
2020-05-01 22:07:11 +0900

    Fix many_threads example

--- example/many_threads.rs
+++ example/many_threads.rs
@@ -1,18 +1,18 @@
 use std::thread;
 use std::time::Duration;
 
-const NUM_THREADS: usize = 100_000;
+const NUM_THREADS: usize = 10_000;
 
 /// Generate many threads so some of them are likely forced to wait for CPU (delays.cpu)
 fn main() {
     let mut handles = Vec::with_capacity(NUM_THREADS);
-    for _ in NUM_THREADS {
+    for _ in 0..NUM_THREADS {
         let th = thread::spawn(|| {
             let mut n = 1;
-            for _ in 0..1_000_000 {
+            for _ in 0..100_000 {
                 n = (n << 1) ^ 17;
             }
-            // TODO: may need to make some side effect to avoid ellision
+            // TODO: may need to make some side effect to avoid elision
             thread::sleep(Duration::from_secs(10));
         });
         handles.push(th);
