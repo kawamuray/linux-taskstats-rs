72a5f8540dd8756ef8f5226147623861e0db3c49
Yuto Kawamura <kawamuray.dadada@gmail.com>
2020-05-25 17:25:43 +0900

    Split docker options and arguments in docker-run.sh

--- docker-build/docker-run.sh
+++ docker-build/docker-run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+opts=""
+while [[ "$1" == '-'* ]]; do
+    opts="$opts $1"
+    shift
+done
+
 root_dir=$(cd $(dirname $0)/..; pwd)
 exec docker run --rm \
      --network host --cap-add NET_ADMIN \
      -v $root_dir:/taskstats \
      -v $root_dir/docker-build/.cargo:/taskstats/.cargo \
+     $opts \
      taskstats-build:latest "$@"
