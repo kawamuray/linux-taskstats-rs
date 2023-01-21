cb27895c4fe2a557cf61725da6c10e6dd6fa01b4
Yuto Kawamura <kawamuray.dadada@gmail.com>
2023-01-21 21:22:17 +0900

    Follow the clap changes

--- src/bin/taskstats/main.rs
+++ src/bin/taskstats/main.rs
@@ -1,32 +1,40 @@
-use clap::{Arg, Command};
+use clap::{Arg, ArgAction, Command};
 use linux_taskstats::format::DefaultHeaderFormat;
-use std::process;
 
 mod cmd;
 
 fn main() {
     let matches = Command::new("A command line interface to Linux taskstats")
-        .arg(Arg::new("verbose").short('v').long("verbose"))
-        .arg(Arg::new("show-delays").short('d').long("delay"))
-        .arg(Arg::new("TIDS").index(1).multiple_values(true))
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verfbose")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("show-delays")
+                .short('d')
+                .long("delay")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("TIDS")
+                .index(1)
+                .num_args(1..)
+                .action(ArgAction::Append),
+        )
         .get_matches();
 
     let tids: Vec<_> = matches
-        .values_of("TIDS")
+        .get_many::<u32>("TIDS")
         .unwrap()
-        .map(|v| match v.parse::<u32>() {
-            Ok(pid) => pid,
-            Err(_) => {
-                eprintln!("Invalid PID: {}", v);
-                process::exit(1);
-            }
-        })
+        .map(|x| *x)
         .collect();
 
     let config = cmd::Config {
         tids,
-        verbose: matches.is_present("verbose"),
-        show_delays: matches.is_present("show-delays"),
+        verbose: matches.contains_id("verbose"),
+        show_delays: matches.contains_id("show-delays"),
         header_format: DefaultHeaderFormat::new(),
     };
     cmd::taskstats_main(config);
